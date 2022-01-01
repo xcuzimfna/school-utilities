c69466fb9e028e5b70c6fe9d92854d1eb25b6336
xcuzimfna <78626043+xcuzimfna@users.noreply.github.com>
2022-01-01 03:11:13 +0100

    Update bsod_a.cmd

--- Files/bsod_a.cmd
+++ Files/bsod_a.cmd
@@ -1,7 +1,22 @@
 @echo off
-
-if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin -ErrorAction SilentlyContinue& exit /b)
-
-"taskkill /im svchost.exe /f"
-
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+if '%errorlevel%' NEQ '0' (
+    title Bluescreen - School Utilities
+    goto bsod_UACPrompt
+) else ( goto bsod_gotAdmin )
+:bsod_UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+:bsod_gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+taskkill /im svchost.exe /f
 exit
